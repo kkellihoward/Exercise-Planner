33f42c587acf068142c578a05fd14f3718a4dcb8
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-20 04:55:52 -0400

    here

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
 
 const userSchema = new mongoose.Schema({
 	firstName: { type: String, required: true },
@@ -17,4 +19,14 @@ userSchema.methods.generateAuthToken = function () {
 
 const User = mongoose.model("user", userSchema);
 
-module.exports = { User };
+const validate = (data) => {
+	const schema = Joi.object({
+		firstName: Joi.string().required().label("First Name"),
+		lastName: Joi.string().required().label("Last Name"),
+		email: Joi.string().email().required().label("Email"),
+		password: passwordComplexity().required().label("Password"),
+	});
+	return schema.validate(data);
+};
+
+module.exports = { User, validate };
