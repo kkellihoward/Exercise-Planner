146e6470fa920918ded7444985dc240d49aee204
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-21 22:05:00 -0400

    adding fixed files

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -65,7 +65,7 @@ export const LoginModal = ({ showModal, setShowModal}) => {
     e.preventDefault();
 
     try {
-      const apiUrl = 'https://exerciseapi-ca661418c8e5.herokuapp.com/user/signin'; // Replace with your API endpoint
+      const apiUrl = 'https://bp-api-87a503314fa5.herokuapp.com/user/signin'; // Replace with your API endpoint
       const data = { email, password };
   
       const response = await axios.post(apiUrl, data, { headers: {
